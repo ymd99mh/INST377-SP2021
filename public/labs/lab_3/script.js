c890a35e3a5d98e83860707d2abc025573d5416e
Maho Yamada <ymd99mh@umd.edu>
2021-03-26 21:39:37 -0400

    test

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,9 +1,7 @@
 /* Put your javascript in here */
-let imgArray = ["images/onigiri_1.png", "images/onigiri_2.png",
-"images/onigiri_3.png","images/onigiri_4.png","images/roll_1.png",
-"images/roll_2.png","images/roll_3.png"];
-
-let counter = 0; 
+const imgArray = document.querySelector('#images');
+console.log(imgArray)
+const counter = 0; 
 
   function nextImage() {
     counter+=3;
@@ -20,14 +18,23 @@ let counter = 0;
     }
 }
 
-  function prevImage(){
+  function prevImage() {
     counter-=3;
     if(counter<0) counter = 0;
-    
-    
         document.getElementById("img1").src = imgArray[counter+0];    
         document.getElementById("img2").src = imgArray[counter+1];    
         document.getElementById("img3").src = imgArray[counter+2];    
-         
+        
+      }
+  function clickToLoadPage() {
+    document.querySelector('.prevImage').addEventListener('click', (event) => {
+      prevImage();
+    });
+    document.querySelector('.nextImage').addEventListener('click', (event) => {
+      nextImage();
+    });
   }
-
+      
+      
+      
+      
